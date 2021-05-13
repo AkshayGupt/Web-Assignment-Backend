fda4a5f2ba22b558c680f0939408880af020c980
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-05-13 23:27:30 +0530

    :card_file_box: Complete controllers for Category

--- src/Controller/Category.php
+++ src/Controller/Category.php
@@ -1,6 +1,8 @@
 <?php
 namespace Src\Controller;
 
+use Src\Utils\Queries;
+
 class Category
 {
     private $db;
@@ -17,59 +19,109 @@ class Category
     // Handle Requests
     public function processRequest()
     {
-        $res = null;
         switch ($this->requestMethod) {
             case 'GET':
                 switch ($this->func) {
                     case 'getAllCategories':
+                        $this->getAllCategories();
                         break;
                     case 'getPopularCategories':
+                        $this->getPopularCategories();
                         break;
                     default:
-                        $res = $this->notFoundResponse("INVALID URL");
-                        break;
-                }
-                break;
-            case 'POST':
-                switch ($this->func) {
-                    case 'createCategory':
-                        break;
-                    default:
-                        $res = $this->notFoundResponse("INVALID URL");
+                        $this->errorResponse("INVALID URL");
                         break;
                 }
                 break;
             case 'PUT':
                 switch ($this->func) {
                     case 'increaseCategoryViewCount':
+                        $this->increaseCategoryViewCount();
                         break;
                     default:
-                        $res = $this->notFoundResponse("INVALID URL");
+                        $this->errorResponse("INVALID URL");
                         break;
                 }
                 break;
             default:
-                $res = $this->notFoundResponse("INVALID URL");
+                $this->errorResponse("INVALID URL");
         }
-        header($res['status_code_header']);
-        echo json_encode($res);
+
+    }
+
+    /**
+     * Fetch all Categories in the database.
+     */
+    private function getAllCategories()
+    {
+        $query = Queries::$getAllCategories;
+
+        try {
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            $this->successResponse($result);
+        } catch (\PDOException$e) {
+            $this->errorResponse($e->getMessage());
+        }
+    }
+
+    /**
+     * Fetch popular Categories in the database.
+     * LIMIT the result top 5 popular categories.
+     */
+    private function getPopularCategories()
+    {
+        $query = Queries::$getPopularCategories;
+
+        try {
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            $this->successResponse($result);
+        } catch (\PDOException$e) {
+            $this->errorResponse($e->getMessage());
+        }
+    }
+
+    /**
+     * Increase the View Count category by 1.
+     * Category identified by [category_id]
+     */
+    private function increaseCategoryViewCount()
+    {
+        $data = (array) json_decode(file_get_contents('php://input'), true);
+        $query = Queries::$increaseCategoryViewCount;
+
+        try {
+            $id = (int) $data['category_id'];
+            $stmt = $this->db->prepare($query);
+            $stmt->execute(array($id));
+
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+            $this->successResponse($result);
+        } catch (\PDOException$e) {
+            $this->errorResponse($e->getMessage());
+        }
     }
 
     // 200 OK response
     private function successResponse($body)
     {
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['status'] = '200';
-        $response['message'] = $body;
-        return $response;
+        $response['data'] = $body;
+        header('HTTP/1.1 200 OK');
+        echo json_encode($response);
     }
 
     // 404 Not Found Response
-    private function notFoundResponse($error)
+    private function errorResponse($error)
     {
         $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
         $response['status'] = '200';
         $response['error'] = $error;
-        return $response;
+        header('HTTP/1.1 404 Not Found');
+        echo json_encode($response);
     }
 }
